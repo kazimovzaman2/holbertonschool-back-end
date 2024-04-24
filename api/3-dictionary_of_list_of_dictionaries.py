269a0463a994df8f28d7ed5d3bd38aef22b3a236
Zaman Kazimov <kazimovzaman2@gmail.com>
2024-04-24 16:52:39 +0400

    Task 3

---
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+Module for tasks
+"""
+import json
+import requests
+from sys import argv
+
+
+def main(id):
+    """
+    Retrieves user information and todos based on the given user ID.
+    """
+    base_url = "https://jsonplaceholder.typicode.com"
+    user_url = "{}/users/{}".format(base_url, id)
+
+    users = requests.get(user_url).json()
+
+    final_dict = {}
+
+    for user in users:
+        todos = requests.get(
+            "{}/todos?userId={}".format(base_url, user.get("id"))
+        ).json()
+        new_user_list = []
+
+        for todo in todos:
+            new_user_list.append(
+                {
+                    "username": user.get("username"),
+                    "task": todo.get("title"),
+                    "completed": todo.get("completed"),
+                }
+            )
+
+            final_dict[user.get("id")] = new_user_list
+
+    with open(f"{id}.json", "w") as f:
+        json.dump(final_dict, f)
+
+
+if __name__ == "__main__":
+    if len(argv) == 2:
+        id = int(argv[1])
+        main(id)
