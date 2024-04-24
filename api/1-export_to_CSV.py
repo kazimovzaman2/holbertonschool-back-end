b2ab7478b7c69d977c700fe117c5ded3f2cc3563
Zaman Kazimov <kazimovzaman2@gmail.com>
2024-04-24 09:24:10 +0400

    csv writer

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -18,12 +18,12 @@ def main(id):
     user = requests.get(user_url).json()
     todos = requests.get(todo_url).json()
 
+    csv_writer = csv.writer(open(f"{id}.csv", "w"), quoting=csv.QUOTE_ALL)
+
     for task in todos:
-        with open(f"{user["id"]}.csv", "a") as f:
-            file = csv.writer(f, quoting=csv.QUOTE_ALL)
-            file.writerow(
-                [user["id"], user["username"], task["completed"], task["title"]]
-            )
+        csv_writer.writerow(
+            [user["id"], user["username"], task["completed"], task["title"]]
+        )
 
 
 if __name__ == "__main__":
